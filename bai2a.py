7bf105b4b75ae1e73125d15be849e4065b339382
datnguyenxuan <82810684+datnguyenxuan@users.noreply.github.com>
2021-04-19 17:33:07 +0700

    Add files via upload

---
+++ bai2a.py
@@ -0,0 +1,8 @@
+n1=int(input("inter n1 value"))
+n2=int(input("inter n2 value"))
+if n1==n2:
+    print("n1,n2 is big")
+elif n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
---
+++ bai2g.py
@@ -0,0 +1,5 @@
+n=int(input("nhap vao mot so:"))
+d=dict()
+for i in range(1,n+1):
+    d[i]=i+1
+print(d)
