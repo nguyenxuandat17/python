4904e21ba010dbe12819c7a4772ab5541cc687bd
datnguyenxuan <82810684+datnguyenxuan@users.noreply.github.com>
2021-04-26 12:52:41 +0700

    Add files via upload

---
+++ bai5 2.py
@@ -0,0 +1,9 @@
+import mymath
+values=[2,4,6,8,10]
+print('square:')
+for v in values:
+    print(mymath.square(v))
+print'cubes:'
+for v in values:
+    print(mymath.cube(v))
+print('average:'+str(mymath.everage(values)))
