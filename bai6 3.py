026ad9190879f82e4c55a586a23aaae66b0f944d
datnguyenxuan <82810684+datnguyenxuan@users.noreply.github.com>
2021-04-26 12:33:51 +0700

    Add files via upload

---
+++ bai6 3.py
@@ -0,0 +1,14 @@
+class nguoi(object):
+    def getGender(seft):
+        return"unknown"
+class nam(nguoi):
+    def getGender((self)):
+        return"nam"
+class nu(nguoi):
+    def getGender((self)):
+        return"nu"
+anam=nam()
+anu=nam()
+print(anam.getGender())
+print(anu.getGender())
+
